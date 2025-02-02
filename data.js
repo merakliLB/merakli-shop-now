63b9c189b9753b1adb9fa4edf91a115853fc5eb8
merakliLB <merakliweb1@gmail.com>
2025-02-02 02:10:12 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,56 +2,38 @@ const products = [
     {
         id: 1,
         name: "قمر رمضان",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
+        description: "hello",  
         price: 80,
         image: "https://imgur.com/trxMBnJ.png"
     },
         {
         id: 2,
         name: "عطر منزل",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
+        description: "hello",  
         price: 80,
         image: "https://imgur.com/IKSoHfC.png"
     },
             {
         id: 3,
         name: "سله ",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
+        description: "hello",  
         price: 80,
         image: "https://imgur.com/ODv06MH.png"
     },
                 {
         id: 4,
         name: "سله كبيره",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
+        description: "hello",  
         price: 80,
         image: "https://imgur.com/G7I904h.png"
     },
                     {
         id: 5,
         name: "بيوت",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
+        description: "hello",  
         price: 80,
         image: "https://imgur.com/0i9cYNI.png"
     },
-                        {
-        id: 6,
-        name: "جاكيت شتوي",
-        description: "hello<br>
-            mar7aba<br>
-            aya shi<br>",  
-        price: 80,
-        image: "https://i.imgur.com/trxMBnJ.png"
-    },
+       
 
 ];
