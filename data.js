da0106761946ebec4cdeb96be1d85bb0d9ebaafb
merakliLB <merakliweb1@gmail.com>
2025-02-02 01:56:12 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,20 +2,56 @@ const products = [
     {
         id: 1,
         name: "قمر رمضان",
-        description: "kkkk10cm aya shi aya shi kmen  aya shi"
-        price:     30,
-        image: "https://i.imgur.com/trxMBnJ.png" // الصورة الجديدة
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://imgur.com/trxMBnJ.png"
     },
-    {
+        {
         id: 2,
         name: "عطر منزل",
-        price:     10,
-        image: "https://imgur.com/IKSoHfC.png" // الصورة الجديدة
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://imgur.com/IKSoHfC.png"
     },
-    {
+            {
         id: 3,
-        name: "سلة العيد",
-        price:     80,
-        image: "https://imgur.com/ODv06MH.png" // الصورة الجديدة
+        name: "سله ",
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://imgur.com/ODv06MH.png"
     },
+                {
+        id: 4,
+        name: "سله كبيره",
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://imgur.com/G7I904h.png"
+    },
+                    {
+        id: 5,
+        name: "بيوت",
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://imgur.com/0i9cYNI.png"
+    },
+                        {
+        id: 6,
+        name: "جاكيت شتوي",
+        description: "hello<br>
+            mar7aba<br>
+            aya shi<br>",  
+        price: 80,
+        image: "https://i.imgur.com/trxMBnJ.png"
+    },
+
 ];
